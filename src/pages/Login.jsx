af684e49618e2dc0832565786c467f47552e0093
ghkdtjsdud0312 <ghkdtjsdud0312@gmail.com>
2023-11-01 13:50:19 +0900

    11/1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,7 @@ import imgLogo from "../images/tier_logo.png";
 import imgBottom from "../images/nedbank_s.png";
 import AxiosApi from "../api/AxiosApi";
 import { Input, Button, Container, Items } from "../component/LoginComponent";
+import Modal from "../util/Modal";
 
 const Login = () => {
   const navigate = useNavigate(); // 화면 이동을 위해서 사용하는 hook
@@ -97,9 +98,9 @@ const Login = () => {
           <Button disabled>SIGN IN</Button>
         )}
       </Items>
-      {/* <Modal open={modalOpen} close={closeModal} header="오류">
+      <Modal open={modalOpen} close={closeModal} header="오류">
         아이디 및 패스워드를 재확인해 주세요.
-      </Modal> */}
+      </Modal>
       <Items className="signup">
         <Link to="/Signup" className="link_style">
           <span>Sign Up</span>
